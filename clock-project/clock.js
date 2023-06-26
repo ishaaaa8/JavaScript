8ab0b996e9c55b6558b61e3ac26ccf443b554c97
Isha singh <115151019+ishaaaa8@users.noreply.github.com>
2023-06-27 00:06:51 +0530

    Add files via upload

---
+++ clock-project/clock.js
@@ -0,0 +1,25 @@
+
+//CREATING CLOCK
+
+const myLabel=document.getElementById("myLabel");
+setInterval(update,1000);
+function update(){
+    let date=new Date();
+    myLabel.innerHTML=formatTime(date);    
+    function formatTime(date){
+        let hours=date.getHours();
+        let mins=date.getMinutes();
+        let seconds=date.getSeconds();
+        let amOrpm=(hours>=12)?"pm":"am";
+        hours=(hours%12)||12;
+
+        hours=formatZeroes(hours);
+        mins=formatZeroes(mins);
+        seconds=formatZeroes(seconds);
+        return `${hours}:${mins}:${seconds} ${amOrpm}`;
+    }
+    function formatZeroes(time){
+        time=time.toString();
+        return time.length<2 ? "0"+time : time;
+    }
+}
